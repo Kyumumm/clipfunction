37b846bd8d9ddbb97c44e5ea5394a6d1a615cbd8
sony <seonkyum@willim.me>
2022-05-02 11:48:21 +0900

    test

---
+++ index.js
@@ -0,0 +1,96 @@
+const firebase = require("firebase/app");
+
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain:  process.env.AUTHDOMAIN,
+  projectId:  process.env.PROJECTID,
+  storageBucket:  process.env.STORAGEBUCKET,
+  messagingSenderId:  process.env.MESSAGEINGSENDERID,
+  appId: process.env.STORAGEID,
+  measurementId: process.env.MEASUREMENTID
+};
+firebase.initializeApp(firebaseConfig)
+
+const express = require("express");
+const app = express();
+const axios = require("axios");
+require("dotenv").config();
+// const {Storage} = require('@google-cloud/storage');
+// const storage = new Storage();
+const Authorization = `Basic ${Buffer.from(`${process.env.CUSTOMERID}:${process.env.CUSTOMER_SECRET}`).toString("base64")}`;
+const fluent_ffmpeg = require('fluent-ffmpeg');
+const { async } = require("@firebase/util");
+
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
+const cwd = path.join(__dirname, '..');
+const destFileName = path.join(cwd, '/fromtoNode/concat/test.mp3')
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage();
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server listening at Port ${port}`));
+
+
+app.get("/down", async(req,res) => {
+    const fileBucket = 'recording-test-c0299.appspot.com'; // The Storage bucket that contains the file.
+    const filePath = 'musicrecording.mp3'; // File path in the bucket.
+
+    const bucket = storage.bucket(fileBucket);
+    await bucket.file(filePath).download({destination: destFileName});
+
+    res.send("Hello World");
+})
+
+app.get("/test", async(req,res) => {
+    res.send("Hello World");
+})
+
+// Makes an ffmpeg command return a promise.
+function promisifyCommand(command) {
+    return new Promise((resolve, reject) => {
+      command.on('end', resolve).on('error', reject).run();
+    });
+  }
+
+app.get("/cut", async(req,res) => {
+    
+    let ffmpeg = require("fluent-ffmpeg");
+    // const targetMP4File =  './fromtoNode/concat/test.mp3';  //영상 파일
+    const targetMP4File =  '/Users/sonykyum/Desktop/fromtoNode/concat/test.mp3';  //영상 파일
+    var array = ['00:01:00','00:10:00']
+    const targetStorageFilePath = 'channel/sid_uid.mp3'
+    const options = {
+    destination: targetStorageFilePath,
+        };
+
+    for(i=0;i<array.length;i++){ 
+        let to_changed_time_mp4 = `/Users/sonykyum/Desktop/fromtoNode/concat/change${i}.mp3`  //영상 파일
+        let command = ffmpeg(targetMP4File)
+            .outputOptions(`-ss ${array[i]}`)
+            .outputOptions("-t 00:00:13")
+            .output(to_changed_time_mp4);
+        
+        await promisifyCommand(command); 
+    }
+
+    res.send("Check File");
+})
+
+app.get("/upload", async(req,res) => {
+    const fileBucket = 'recording-test-c0299.appspot.com'; // The Storage bucket that contains the file.  
+    let to_changed_time_mp4 = '/Users/sonykyum/Desktop/fromtoNode/concat/test.mp4'
+    const targetStorageFilePath = 'channel/sid_uid.mp3'
+    const options = {
+        destination: targetStorageFilePath,
+            };
+    // Uploading the audio.
+    storage.bucket(fileBucket).upload(to_changed_time_mp4, {destination: options});
+
+    res.send("Send End");
+})
